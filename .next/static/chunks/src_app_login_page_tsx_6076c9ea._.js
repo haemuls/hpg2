4930cef2e0ec5a5b779be3491bc62866945a7ebf
haemuls <icechicken127@gmail.com>
2025-05-12 14:07:53 +0900

    12

--- .next/static/chunks/src_app_login_page_tsx_6076c9ea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_login_login1_module_c3dbec78.css",
-  "static/chunks/src_app_login_35e39b89._.js"
-],
-    source: "dynamic"
-});
